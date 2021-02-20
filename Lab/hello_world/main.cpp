6d15f162b3315a2376a28f96c126009578eaca71
Antero <anteroavalos2508@gmail.com>
2021-02-20 12:37:06 -0800

    hello world
    highly complex

---
+++ Lab/hello_world/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:        	main.cpp
+ * Author:      	Antero Avalos
+ * Date Created:    	02/20/21 11:10 am
+ * Date Finished:       02/20/21 
+ * Purpose:     	Hello World Output 
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    //Output hello world
+    cout << "Hello World" << endl; 
+    
+    return 0;
+}
